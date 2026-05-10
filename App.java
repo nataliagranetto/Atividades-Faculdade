38a0c84a081b97dc92ebce66f54010d87788d5f5
Natália Reis Granetto <nataliareisgranetto@gmail.com>
2026-05-10 20:19:16 -0300

    exercício finalizado

--- App.java
+++ App.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        String nome;
+        Cliente cliente1 = new Cliente();
+        Pedido pedido1 = new Pedido();
 
-        System.out.println("Digite seu nome e faça seu pedido: ");
-        nome = sc.nextLine();
+        System.out.println("Digite seu nome:");
+        cliente1.setNome(sc.nextLine());
 
-        Cliente cliente1 = new Pedido();
+        System.out.println("Digite a quantidade:");
+        cliente1.setQuantidade(sc.nextInt());
+
+        System.out.println("  DADOS DO PEDIDO  ");
+        System.out.println("Cliente: " + cliente1.getNome());
+        System.out.println("Quantidade: " + cliente1.getQuantidade());
+        System.out.println("Número do pedido: " + pedido1.getNumeroDoPedido());
+
+        sc.close();
     }
 }
